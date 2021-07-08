c7d3d239f2fbcf2cbd61e95be37463ee1de95cb1
interceptor128 <polestar.interceptor.0128@gmail.com>
2021-07-08 19:26:54 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# Collatz Sequence
+def collatz(number):
+    if number == 1:
+        return 0
+
+    if number % 2 == 0:
+        return int(number / 2)
+    else:
+        return int(number * 3 + 1)
+
+number = int(input())
+print(number)
+while number != 1:
+    number = collatz(number)
+    print(number)
+ 
